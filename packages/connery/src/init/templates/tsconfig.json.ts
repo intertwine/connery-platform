3562eb97b77d05df768eab0f664604206ef06ff5
Bryan Young <bryan@intertwinesys.com>
2024-01-09 12:07:07 -0500

    yarn tsconfig updates

--- packages/connery/src/init/templates/tsconfig.json.ts
+++ packages/connery/src/init/templates/tsconfig.json.ts
@@ -1,7 +1,7 @@
 export default `{
   "compilerOptions": {
     "target": "esnext",
-    "module": "nodenext",
+    "module": "NodeNext",
     "esModuleInterop": true
   },
   "include": ["src/index.ts"]
